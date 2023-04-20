bcc4883c297fc104b193fdb8698fd6d5a2db2c37
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-04-20 21:58:13 +0300

    finish comment screen

--- Screens/NestedPostsScreen/CommentsScreen.js
+++ Screens/NestedPostsScreen/CommentsScreen.js
@@ -7,6 +7,10 @@ import {
   TouchableOpacity,
   SafeAreaView,
   FlatList,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { Dimensions } from "react-native";
 import { SendMessageSvg } from "../../Utils/SvgComponents";
@@ -28,10 +32,20 @@ const width = Dimensions.get("window").width;
 export const CommentsScreen = ({ route: { params } }) => {
   const [comment, setComment] = useState("");
   const [allComments, setAllComments] = useState(null);
+  const [isKeyboard, setIsKeyboard] = useState(false);
 
   const nickName = useSelector((state) => state.auth.nickName);
 
   const { docId, photo } = params.item;
+
+  const handleKeyboard = () => {
+    setIsKeyboard(true);
+  };
+
+  const hideKeyboard = () => {
+    setIsKeyboard(false);
+    Keyboard.dismiss();
+  };
 
   const getAllComments = async () => {
     let resData = [];
@@ -69,6 +83,7 @@ export const CommentsScreen = ({ route: { params } }) => {
     const postRef = await doc(cloudFireStore, "userPosts", docId);
     await updateDoc(postRef, { commentsCount: count.data().count });
     setComment("");
+    hideKeyboard();
     getAllComments();
   };
 
@@ -77,56 +92,77 @@ export const CommentsScreen = ({ route: { params } }) => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.postContainer}>
-        <Image
-          style={styles.postImg}
-          source={{ uri: photo }}
-        />
-      </View>
-      <SafeAreaView>
-        <FlatList
-          data={allComments}
-          renderItem={({ item, index }) => (
-            <View style={styles.commentView}>
-              <View
-                style={{
-                  ...styles.commentWrapper,
-                  marginLeft: index % 2 !== 0 ? "auto" : 0,
-                }}>
-                <Text style={styles.comment}>{item.comment}</Text>
-              </View>
-            </View>
-          )}
-          keyExtractor={(item) => item.id}
-        />
-      </SafeAreaView>
-      <View style={styles.inputWrapper}>
-        <TextInput
-          value={comment}
-          onSubmitEditing={createComment}
-          onChangeText={(e) => setComment(e)}
-          style={styles.input}
-          inputMode="text"
-          maxLength={200}
-          multiline
-          placeholder="Comment..."
-        />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <View style={{ ...styles.container }}>
         <TouchableOpacity
-          onPress={createComment}
-          style={styles.sendBtn}
-          activeOpacity={0.6}>
-          <SendMessageSvg />
+          onPress={hideKeyboard}
+          activeOpacity={1}
+          style={styles.postContainer}>
+          <Image
+            style={styles.postImg}
+            source={{ uri: photo }}
+          />
         </TouchableOpacity>
+        <SafeAreaView>
+          <FlatList
+            ListEmptyComponent={
+              <Image source={require("../../assets/images/noComments.jpg")} />
+            }
+            style={{ marginBottom: 290 }}
+            data={allComments}
+            renderItem={({ item, index }) => (
+              <View style={styles.commentView}>
+                <View
+                  style={{
+                    ...styles.commentWrapper,
+                    marginLeft: index % 2 !== 0 ? "auto" : 0,
+                  }}>
+                  <Text style={styles.comment}>{item.comment}</Text>
+                  <Text
+                    style={{
+                      ...styles.date,
+                      marginLeft: index % 2 !== 0 ? "auto" : 0,
+                    }}>
+                    {new Date(item.date).toLocaleString()}
+                  </Text>
+                </View>
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        </SafeAreaView>
+
+        <View style={styles.inputWrapper}>
+          <TextInput
+            value={comment}
+            onFocus={handleKeyboard}
+            onSubmitEditing={createComment}
+            onChangeText={(e) => setComment(e)}
+            style={styles.input}
+            inputMode="text"
+            maxLength={200}
+            multiline
+            placeholder="Comment..."
+          />
+          <TouchableOpacity
+            onPress={createComment}
+            style={styles.sendBtn}
+            activeOpacity={0.6}>
+            <SendMessageSvg />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     paddingBottom: 100,
+    backgroundColor: "#fff",
   },
   postContainer: {
     marginHorizontal: 16,
@@ -147,13 +183,21 @@ const styles = StyleSheet.create({
   commentWrapper: {
     width: 300,
     marginBottom: 24,
+    backgroundColor: "#E8E8E8",
     borderWidth: 1,
+    borderRadius: 6,
     borderColor: "#E8E8E8",
     paddingHorizontal: 16,
     paddingVertical: 16,
   },
   comment: {
     fontFamily: "JetBrainsMono",
+  },
+  date: {
+    fontFamily: "JetBrainsMono",
+    marginTop: 8,
+    fontSize: 10,
+    color: "#BDBDBD",
   },
   input: {
     fontFamily: "JetBrainsMono",
